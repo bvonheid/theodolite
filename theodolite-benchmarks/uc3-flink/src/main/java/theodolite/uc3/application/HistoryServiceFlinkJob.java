ebbd897cb47e64f7de1453e3732af1873fffd3a0
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2021-07-13 11:14:22 +0200

    Add scotty to flinks uc3

--- theodolite-benchmarks/uc3-flink/src/main/java/theodolite/uc3/application/HistoryServiceFlinkJob.java
+++ theodolite-benchmarks/uc3-flink/src/main/java/theodolite/uc3/application/HistoryServiceFlinkJob.java
@@ -1,6 +1,10 @@
 package theodolite.uc3.application;
 
 import com.google.common.math.Stats;
+import de.tub.dima.scotty.core.AggregateWindow;
+import de.tub.dima.scotty.core.windowType.SlidingWindow;
+import de.tub.dima.scotty.core.windowType.WindowMeasure;
+import de.tub.dima.scotty.flinkconnector.KeyedScottyWindowOperator;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -11,11 +15,14 @@ import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.util.Collector;
 import org.apache.kafka.common.serialization.Serdes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -97,6 +104,8 @@ public final class HistoryServiceFlinkJob {
     final String inputTopic = this.config.getString(ConfigurationKeys.KAFKA_INPUT_TOPIC);
     final String outputTopic = this.config.getString(ConfigurationKeys.KAFKA_OUTPUT_TOPIC);
     final ZoneId timeZone = ZoneId.of(this.config.getString(ConfigurationKeys.TIME_ZONE));
+    final String windowProcessor =
+        this.config.getString(ConfigurationKeys.WINDOW_PROCESSOR, "default");
     final Time aggregationDuration =
         Time.milliseconds(Duration
             .parse(this.config.getString(ConfigurationKeys.AGGREGATION_DURATION)).toMillis());
@@ -119,26 +128,64 @@ public final class HistoryServiceFlinkJob {
 
     // Streaming topology
     final StatsKeyFactory<HourOfDayKey> keyFactory = new HourOfDayKeyFactory();
-    this.env
+
+    final KeyedStream<ActivePowerRecord, HourOfDayKey> newKeyStream = this.env
         .addSource(kafkaSource).name("[Kafka Consumer] Topic: " + inputTopic)
         // .rebalance()
         .keyBy((KeySelector<ActivePowerRecord, HourOfDayKey>) record -> {
           final Instant instant = Instant.ofEpochMilli(record.getTimestamp());
           final LocalDateTime dateTime = LocalDateTime.ofInstant(instant, timeZone);
           return keyFactory.createKey(record.getIdentifier(), dateTime);
-        })
-        .window(SlidingEventTimeWindows.of(aggregationDuration, aggregationAdvance))
-        .aggregate(new StatsAggregateFunction(), new HourOfDayProcessWindowFunction())
-        .map(tuple -> {
-          final String newKey = keyFactory.getSensorId(tuple.f0);
-          final String newValue = tuple.f1.toString();
-          final int hourOfDay = tuple.f0.getHourOfDay();
-          LOGGER.info("{}|{}: {}", newKey, hourOfDay, newValue);
-          return new Tuple2<>(newKey, newValue);
-        })
-        .name("map")
+        });
+
+    SingleOutputStreamOperator<Tuple2<String, String>> resultStream;
+    final StatsAggregateFunction aggFunction = new StatsAggregateFunction();
+
+    if ("scotty".equals(windowProcessor)) {
+      LOGGER.info("Use Scotty Window Function with {}ms window time and {}ms advance time",
+          aggregationDuration.toMilliseconds(), aggregationAdvance.toMilliseconds());
+
+      // Scotty configuration
+      final SlidingWindow slidingWindow = new SlidingWindow(WindowMeasure.Time,
+          aggregationDuration.toMilliseconds(), aggregationAdvance.toMilliseconds());
+      final KeyedScottyWindowOperator<HourOfDayKey, ActivePowerRecord, Stats> processingFunction =
+          new KeyedScottyWindowOperator<>(aggFunction);
+      processingFunction.addWindow(slidingWindow);
+
+      // Process Stream
+      resultStream =
+          newKeyStream
+              .process(processingFunction)
+              .flatMap(
+                  (final AggregateWindow<Stats> aggWindow,
+                      final Collector<Tuple2<String, String>> out) -> {
+                    aggWindow.getAggValues()
+                        .forEach(stats -> out.collect(new Tuple2<>("no_key", stats.toString())));
+                  })
+              .name("flatMap");
+    } else {
+      LOGGER.info("Use KStreams Window Function with {}ms window time and {}ms advance time",
+          aggregationDuration.toMilliseconds(), aggregationAdvance.toMilliseconds());
+
+      // Process Stream
+      resultStream = newKeyStream
+          .window(SlidingEventTimeWindows.of(aggregationDuration, aggregationAdvance))
+          .aggregate(aggFunction, new HourOfDayProcessWindowFunction())
+          .map(tuple -> {
+            final String newKey = keyFactory.getSensorId(tuple.f0);
+            final String newValue = tuple.f1.toString();
+            final int hourOfDay = tuple.f0.getHourOfDay();
+            LOGGER.info("{}|{}: {}", newKey, hourOfDay, newValue);
+            return new Tuple2<>(newKey, newValue);
+          })
+          .name("map");
+    }
+
+    // Write results to output stream
+    resultStream
         .returns(Types.TUPLE(Types.STRING, Types.STRING))
-        .addSink(kafkaSink).name("[Kafka Producer] Topic: " + outputTopic);
+        .addSink(kafkaSink)
+        .name("[Kafka Producer] Topic: " + outputTopic);
   }
 
   /**
--- theodolite-benchmarks/uc3-flink/src/main/java/theodolite/uc3/application/StatsAggregateFunction.java
+++ theodolite-benchmarks/uc3-flink/src/main/java/theodolite/uc3/application/StatsAggregateFunction.java
@@ -6,12 +6,17 @@ import org.apache.flink.api.common.functions.AggregateFunction;
 import theodolite.uc3.application.util.StatsFactory;
 import titan.ccp.model.records.ActivePowerRecord;
 
+
 /**
  * Statistical aggregation of {@link ActivePowerRecord}s using {@link Stats}.
  */
-public class StatsAggregateFunction implements AggregateFunction<ActivePowerRecord, Stats, Stats> {
+public class StatsAggregateFunction
+    implements AggregateFunction<ActivePowerRecord, Stats, Stats>,
+    de.tub.dima.scotty.core.windowFunction.AggregateFunction<ActivePowerRecord, Stats, Stats> {
 
   private static final long serialVersionUID = -8873572990921515499L; // NOPMD
+
+  // Flink Aggregate Function methods
 
   @Override
   public Stats createAccumulator() {
@@ -34,5 +39,28 @@ public class StatsAggregateFunction implements AggregateFunction<ActivePowerReco
     statsAccumulator.addAll(a);
     statsAccumulator.addAll(b);
     return statsAccumulator.snapshot();
+  }
+
+
+  // Scotty Aggregate Function methods
+
+  @Override
+  public Stats lift(final ActivePowerRecord apr) {
+    final StatsAccumulator statsAccumulator = new StatsAccumulator();
+    statsAccumulator.add(apr.getValueInW());
+    return statsAccumulator.snapshot();
+  }
+
+  @Override
+  public Stats combine(final Stats s1, final Stats s2) {
+    final StatsAccumulator statsAccumulator = new StatsAccumulator();
+    statsAccumulator.addAll(s1);
+    statsAccumulator.addAll(s2);
+    return statsAccumulator.snapshot();
+  }
+
+  @Override
+  public Stats lower(final Stats s) {
+    return s;
   }
 }
